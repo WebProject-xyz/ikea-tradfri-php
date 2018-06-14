eb11f60e22dcd54ab3818245d8e60ba22201acf1
Benjamin Fahl <git@fahl-design.de>
2018-06-15 01:12:43 +0200

    - fixed coverage paths

--- build/script/travis_after_success.sh
+++ build/script/travis_after_success.sh
@@ -3,5 +3,5 @@ set -e
 trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
 
 ## coverage reports
-php vendor/bin/codacycoverage clover tests/_output/coverage.xml
+php vendor/bin/codacycoverage clover build/logs/clover.xml
 if [[ $RUN_WITH_COVERAGE == 'true' && "$TRAVIS_PULL_REQUEST" == "false" ]]; then ./cc-test-reporter after-build -t=clover build/logs/clover.xml --exit-code $TRAVIS_TEST_RESULT ; fi
