aab1897037e5b666d87b6865335ed4253e3771b3
Benjamin Fahl <git@fahl-design.de>
2018-06-16 01:41:21 +0200

    - travis env magic...

--- build/script/travis_after_success.sh
+++ build/script/travis_after_success.sh
@@ -3,5 +3,5 @@ set -e
 trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
 
 ## coverage reports
-php vendor/bin/codacycoverage clover build/logs/clover.xml
+if [[ $RUN_WITH_COVERAGE == 'true' ]]; then php vendor/bin/codacycoverage clover build/logs/clover.xml ; fi
 if [[ $RUN_WITH_COVERAGE == 'true' && "$TRAVIS_PULL_REQUEST" == "false" ]]; then ./cc-test-reporter after-build -t=clover build/logs/clover.xml --exit-code $TRAVIS_TEST_RESULT ; fi
