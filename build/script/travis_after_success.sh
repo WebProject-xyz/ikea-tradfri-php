564042dfa3b3f2fc82aea1db0caf09693dce151c
Benjamin Fahl <git@fahl-design.de>
2022-03-17 21:44:38 +0100

    [qa] update unit tests and tes setup
    - verify mock calls
    - cleanup

--- build/script/travis_after_success.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-trap '>&2 echo Error: Command \`$BASH_COMMAND\` on line $LINENO failed with exit code $?' ERR
-
-## coverage reports
-if [[ $RUN_WITH_COVERAGE == 'true' ]]; then php vendor/bin/codacycoverage clover build/logs/clover.xml ; fi
-if [[ $RUN_WITH_COVERAGE == 'true' && "$TRAVIS_PULL_REQUEST" == "false" ]]; then ./cc-test-reporter after-build -t=clover build/logs/clover.xml --exit-code $TRAVIS_TEST_RESULT ; fi
